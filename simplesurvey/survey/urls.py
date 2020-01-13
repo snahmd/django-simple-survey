0265d19556a6891bfb3d128e3b42b9570110050b
snahmd <sanahmed7@gmail.com>
2020-01-13 22:41:16 +0100

    adding more views to panel

--- simplesurvey/survey/urls.py
+++ simplesurvey/survey/urls.py
@@ -6,5 +6,6 @@ urlpatterns =patterns('',
   url(r'^survey/(?P<survey_id>\d+)/$', views.survey_view, name="survey-detail"),
   url(r'^admin_login/$', views.admin_login, name="admin-login"),
   url(r'^admin_panel/$', views.admin_panel, name="admin-panel"),
-  url(r'^admin_panel/survey_delete/$', views.survey_delete, name="admin-survey-delete")
+  url(r'^admin_panel/survey_delete/$', views.survey_delete, name="admin-survey-delete"),
+  url(r'^admin_panel/survey/(?P<survey_id>\d+)/$', views.admin_answers, name="answer-detail"),
 )
--- simplesurvey/survey/views.py
+++ simplesurvey/survey/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from models import Survey, SurveyAnswer
+from models import Survey, SurveyAnswer, Question
 from django.contrib.auth import authenticate, login
 # Create your views here.
 def index(request):
@@ -55,4 +55,28 @@ def survey_delete(request):
   sv_del.delete()
   return redirect('admin-panel')
 
-  
+def admin_answers(request, survey_id):
+  survey = Survey.objects.get(id=survey_id)
+  answers = survey.surveyanswer_set.all()
+  ctx = {'answers': answers, 'survey':survey}
+  return render(request, 'admin-survey-detail.html', ctx)
+
+def survey_create_view(request):
+  return render(request, 'survey-create.html',{})
+
+def question_add_view(request):
+  return render(request, 'question-add.html',{})
+
+def choice_add_view(request):
+  question = Question.objects.get(id=int(request.session['current_question']))
+  return render(request, 'choice-add.html',{'question':question})
+
+def survey_create(request):
+  newSurvey = Survey()
+  newSurvey.title = request.POST['survey_title']
+  newSurvey.save()
+  request.session['current_survey'] = newSurvey.id
+
+
+
+ 
